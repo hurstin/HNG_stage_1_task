0e049eb8b8c46988b33f675a7f721678f4f9b0f8
hurstin <132925282+hurstin@users.noreply.github.com>
2025-10-21 18:34:06 +0100

    added controller file

--- route.js
+++ route.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { hello } from './controller.js';
 
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-  res.send('hello world');
-});
+router.route('/').get(hello);
 
 export default router;
